8209091272a7bd59df7fe2aa07370f3906cf7c2b
Dmitriy Frolov <dmitriy.a.frolov@gmail.com>
2023-10-09 23:04:09 +0300

    Make the api_key attribute private

--- src/channel.py
+++ src/channel.py
@@ -8,7 +8,7 @@ class Channel:
     """Класс для ютуб-канала"""
 
     # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-    api_key: str = os.getenv('YT_API_KEY')
+    __api_key: str = os.getenv('YT_API_KEY')
 
     # создать специальный объект для работы с API
     youtube = build('youtube', 'v3', developerKey=api_key)
