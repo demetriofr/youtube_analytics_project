287f7acb63c3c3144cc4fa14289ce461ea9052f8
Dmitriy Frolov <dmitriy.a.frolov@gmail.com>
2023-10-16 23:27:07 +0300

    Fix channel.py The get_info_about_channel method was created and the channel attribute was added during initialization, and the print_info method was simplified.

--- src/channel.py
+++ src/channel.py
@@ -16,8 +16,6 @@ class Channel:
         __api_key: str = os.getenv('YT_API_KEY')
         return __api_key
 
-    __API_KEY = __api_key()
-
     @staticmethod
     def youtube(api_key):
         """
@@ -26,14 +24,25 @@ class Channel:
         youtube = build('youtube', 'v3', developerKey=api_key)
         return youtube
 
+    __API_KEY = __api_key()
     YOUTUBE = youtube(__API_KEY)
 
+
+
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """
+        Экземпляр инициализируется id канала и добавляет данные по API
+        """
         self.channel_id = channel_id
+        self.channel = self.get_info_about_channel(channel_id)
+
+    def get_info_about_channel(self, channel_id):
+        """
+        Получает информацию о канале
+        """
+        channel = self.YOUTUBE.channels().list(id=channel_id, part='snippet,statistics').execute()
+        return channel
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        channel = self.YOUTUBE.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        # Выводит словарь в json-подобном удобном формате с отступами
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        """Выводит информацию о канале в словарь в JSON формате с отступами"""
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
