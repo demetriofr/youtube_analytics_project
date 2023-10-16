544199475211c155daed1f4dbe6efb0f08b64214
Dmitriy Frolov <dmitriy.a.frolov@gmail.com>
2023-10-16 22:26:49 +0300

    Make the private variable __api_key the @staticmethod __api_key

--- src/channel.py
+++ src/channel.py
@@ -7,11 +7,17 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
 
-    # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-    __api_key: str = os.getenv('YT_API_KEY')
+    @staticmethod
+    def __api_key():
+        """
+        Добавляет API key YouTube из переменного окружения операционной системы
+        """
+        # YT_API_KEY копируется из Google и добавляется в переменные окружения
+        __api_key: str = os.getenv('YT_API_KEY')
+        return __api_key
 
     # создать специальный объект для работы с API
-    youtube = build('youtube', 'v3', developerKey=api_key)
+    youtube = build('youtube', 'v3', developerKey=__api_key())
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
