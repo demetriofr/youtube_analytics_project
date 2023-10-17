49c7347671f966b8980c6a4cb7b3d85a17523801
Dmitriy Frolov <dmitriy.a.frolov@gmail.com>
2023-10-17 05:03:21 +0300

    add tests according to the TDD approach

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.channel import Channel
 
 
@@ -7,4 +9,34 @@ def test_channel_id():
 
 
 def test_print_info():
+    pass
+
+
+def test_channel_attribute():
+    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    assert moscowpython.channel_id == 'UC-OVMPlMA3-YCIeg4z5z23A'
+    assert moscowpython.title == 'MoscowPython'
+    assert moscowpython.description == '''Видеозаписи со встреч питонистов и джангистов в Москве и не только. :)
+Присоединяйтесь: https://www.facebook.com/groups/MoscowDjango! :)'''
+    assert moscowpython.url == 'https://www.youtube.com/@moscowdjangoru'
+    assert moscowpython.subscriber_count >= 26600
+    assert moscowpython.video_count >= 709
+    assert moscowpython.view_count >= 2432150
+
+
+@pytest.mark.xfail(raises=AttributeError)
+def test_channel_attribute__channel_id():
+    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    moscowpython.channel_id = 'Новое название'
+
+
+def test_channel__get_service():
+    assert str(Channel.get_service())[1:45] == 'googleapiclient.discovery.Resource object at'
+
+
+def test_get_service():
+    pass
+
+
+def test_channel__to_json():
     pass
